857f4b8aea98ff8328fd2bf4ac4e7f0f925e2a7a
teerapol <teerapol.thanamahamongkol@email.thns.co.th>
2018-12-08 15:57:39 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.end("teerapol");
+})
+
+
+app.get('/print/:name', (req, res)=>{
+    res.end("name: "+req.params.name);
+})
+
+
+app.listen(3000, ()=>{
+    console.log("server running")
+})
